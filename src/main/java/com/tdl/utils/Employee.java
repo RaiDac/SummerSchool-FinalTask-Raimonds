a23e057659c0552482bec2b327ad2d4ae11ab786
Raimonds Dacis <dacisraimonds@gmail.com>
2025-08-15 14:28:33 +0300

    Added protected methods to Employee class

--- src/main/java/com/tdl/utils/Employee.java
+++ src/main/java/com/tdl/utils/Employee.java
@@ -6,4 +6,28 @@ public class Employee {
     private String name;
     private String surname;
     private Role role;
+
+    protected int getId() {
+        return id;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected String getSurname() {
+        return surname;
+    }
+
+    protected Role getRole() {
+        return role;
+    }
+
+    protected void setName(String newName) {
+        name = newName;
+    }
+
+    protected void setSurname(String newSurname) {
+        surname = newSurname;
+    }
 }
